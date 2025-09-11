15e03754661c18320696527d93b7da27af6efbb9
Gleb Tolstik <glebtolst1k@mail.ru>
2025-09-11 18:35:36 +0300

    add SearchService class

--- src/main/java/by/hobbygames/api/SearchService.java
+++ src/main/java/by/hobbygames/api/SearchService.java
@@ -1,5 +1,9 @@
 package by.hobbygames.api;
 
+import io.restassured.response.Response;
+
 public class SearchService {
+    private final String URL = "https://hobbygames.by/catalog/search";
+    private Response response;
 
 }
